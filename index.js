e3ab42de052e6454ba69391f1f474f06acec2fe6
brianko <kobj11@gmail.com>
2021-12-22 15:33:00 -0500

    commit tree 61697b2aa55b3091553e65aa7d0066aaa35f6afb

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let names = ["Guadalupe", "Ollie", "Aki"]
+let event = "surprise"
+function writeCards(names,event) {
+    let newMessage = []
+ for(let i = 0; i < names.length; i++) {
+    newMessage.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    };
+    return newMessage
+}
+function countDown(n) {
+    console.log(n)
+    while (n>0) {
+        n -= 1;
+        console.log(n)
+    }
+}
